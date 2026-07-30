87dc432136b40e79ecc1091d52a448e1997d3147
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2026-07-30 17:51:00 +0100

    Improve filenames and export new Public names

--- monocle/check_metadata.sh
+++ monocle/check_metadata.sh
@@ -7,7 +7,8 @@ fi
 
 OLD_METADATA="$1"
 NEW_METADATA="$2"
-OLD_METADATA_REMOVED=$(sed -E 's/.csv$/-removed.txt/' <<< ${OLD_METADATA})
+OLD_METADATA_REMOVED=$(sed -E 's/.csv$/-removed-publicnames.txt/' <<< ${OLD_METADATA})
+NEW_METADATA_NEW=$(sed -E 's/.csv$/-new-publicnames.txt/' <<< ${NEW_METADATA})
 NEW_METADATA_CHANGE=$(sed -E 's/.csv$/-change.csv/' <<< ${NEW_METADATA})
 
 # Extract removed Public_name(s)
@@ -16,6 +17,14 @@ if [[ -n $(comm -23 <(cut -d',' -f 2 ${OLD_METADATA} | sort) <(cut -d',' -f 2 ${
 	comm -23 <(cut -d',' -f 2 ${OLD_METADATA} | sort) <(cut -d',' -f 2 ${NEW_METADATA} | sort) > ${OLD_METADATA_REMOVED}
 else
 	echo "No Public_name(s) is removed."
+fi
+
+# Extract new Public_name(s)
+if [[ -n $(comm -13 <(cut -d',' -f 2 ${OLD_METADATA} | sort) <(cut -d',' -f 2 ${NEW_METADATA} | sort)) ]]; then
+	echo "Public_name(s) added. Saving new Public_name(s) to ${NEW_METADATA_NEW}"
+	comm -13 <(cut -d',' -f 2 ${OLD_METADATA} | sort) <(cut -d',' -f 2 ${NEW_METADATA} | sort) > ${NEW_METADATA_NEW}
+else
+	echo "No Public_name(s) is added."
 fi
 
 if [[ $(head -n 1 ${OLD_METADATA}) != $(head -n 1 ${NEW_METADATA}) ]]; then
