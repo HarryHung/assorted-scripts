8e7bb995e857bef2e0ac73d0c649ae21dae5cc45
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2026-07-30 16:01:20 +0100

    Improve comments

--- monocle/check_published.sh
+++ monocle/check_published.sh
@@ -5,11 +5,12 @@ if [ "$1" == "-h" ] || [ -z "$1" ]; then
   exit 0
 fi
 
-# Extract new published samples
 OLD_PUBLISHED="$1"
 NEW_PUBLISHED="$2"
 OLD_PUBLISHED_REMOVED=$(sed -E 's/.txt$/-removed.txt/' <<< ${OLD_PUBLISHED})
 NEW_PUBLISHED_NEW=$(sed -E 's/.txt$/-new.txt/' <<< ${NEW_PUBLISHED})
+
+# Extract removed published samples
 if [[ -n $(comm -23 <(sort ${OLD_PUBLISHED}) <(sort ${NEW_PUBLISHED})) ]]; then
 	echo "Published sample(s) removed. Saving removed sample(s) to ${OLD_PUBLISHED_REMOVED}"
 	comm -23 <(sort ${OLD_PUBLISHED}) <(sort ${NEW_PUBLISHED}) > ${OLD_PUBLISHED_REMOVED}
@@ -17,6 +18,7 @@ else
 	echo "No published sample is removed."
 fi
 
+# Extract new published samples
 if [[ -n $(comm -13 <(sort ${OLD_PUBLISHED}) <(sort ${NEW_PUBLISHED})) ]]; then
 	echo "New published sample(s) is found. Saving new sample(s) to ${NEW_PUBLISHED_NEW}"
 	comm -13 <(sort ${OLD_PUBLISHED}) <(sort ${NEW_PUBLISHED}) > ${NEW_PUBLISHED_NEW}
