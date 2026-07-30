0f8d05a0c0fbb73107ef36c54671991c0db94370
Harry Hung <4848896+HarryHung@users.noreply.github.com>
2026-07-30 16:09:09 +0100

    Improve messages and comments

--- monocle/check_published.sh
+++ monocle/check_published.sh
@@ -10,16 +10,18 @@ NEW_PUBLISHED="$2"
 OLD_PUBLISHED_REMOVED=$(sed -E 's/.txt$/-removed.txt/' <<< ${OLD_PUBLISHED})
 NEW_PUBLISHED_NEW=$(sed -E 's/.txt$/-new.txt/' <<< ${NEW_PUBLISHED})
 
-# Extract removed published samples
+# Extract removed published Public_name(s)
 if [[ -n $(comm -23 <(sort ${OLD_PUBLISHED}) <(sort ${NEW_PUBLISHED})) ]]; then
-	echo "Published sample(s) removed. Saving removed sample(s) to ${OLD_PUBLISHED_REMOVED}"
+	echo "Published Public_name(s) removed. Saving removed Public_name(s) to ${OLD_PUBLISHED_REMOVED}"
 	comm -23 <(sort ${OLD_PUBLISHED}) <(sort ${NEW_PUBLISHED}) > ${OLD_PUBLISHED_REMOVED}
 else
-	echo "No published sample is removed."
+	echo "No published Public_name(s) is removed."
 fi
 
-# Extract new published samples
+# Extract new published Public_name(s)
 if [[ -n $(comm -13 <(sort ${OLD_PUBLISHED}) <(sort ${NEW_PUBLISHED})) ]]; then
-	echo "New published sample(s) is found. Saving new sample(s) to ${NEW_PUBLISHED_NEW}"
+	echo "New published Public_name(s) is found. Saving new Public_name(s) to ${NEW_PUBLISHED_NEW}"
 	comm -13 <(sort ${OLD_PUBLISHED}) <(sort ${NEW_PUBLISHED}) > ${NEW_PUBLISHED_NEW}
+else
+	echo "No new published Public_name(s) is found."
 fi
